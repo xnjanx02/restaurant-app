2512fc36757956e12b3b3aceb25ef0010ede1e9c
xnjanx02 <anjana.jk009@gmail.com>
2022-07-07 21:49:49 +0530

    New component login page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { ViewFoodComponent } from './view-food/view-food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    ViewFoodComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-food/view-food.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-food',
+  templateUrl: './view-food.component.html',
+  styleUrls: ['./view-food.component.css']
+})
+export class ViewFoodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
