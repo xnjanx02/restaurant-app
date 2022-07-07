1bf27d09a60c62a4ab75ff1def006ce28dce5158
xnjanx02 <anjana.jk009@gmail.com>
2022-07-07 22:30:29 +0530

    New component payment added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ViewFoodComponent } from './view-food/view-food.component';
 import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     ViewFoodComponent,
-    SearchRestaurantComponent
+    SearchRestaurantComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
